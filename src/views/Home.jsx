084910c24dde2b9397e10696bb6d07c87849942e
Marksolidor <marco350sl@gmail.com>
2023-03-09 07:29:32 -0300

    feat home event rendering

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,52 +1,54 @@
 import React, { useState, useEffect } from "react";
-import UpcomingEventCard from "../components/Upcomming_Events";
+import { NavLink } from "react-router-dom";
 
 function Home() {
   const [events, setEvents] = useState([]);
 
   useEffect(() => {
-    async function fetchData() {
-      const res = await fetch("/events.json");
-      res
-        .json()
-        .then((res) => setEvents(res))
-        .catch((err) => console.log(err));
-    }
-
-    fetchData();
+    jsonData();
   }, []);
 
-  const formatDate = (date) => {
-    const options = { year: "numeric", month: "long", day: "numeric" };
-    return new Date(date).toLocaleDateString(undefined, options);
+  const jsonData = async () => {
+    try {
+      const response = await fetch("/events.json");
+      const data = await response.json();
+      setEvents(data);
+    } catch (e) {
+      alert("Mensaje del Servidor: No se pudo establecer la conexión");
+    }
   };
-
-  const getUpcomingEvents = () => {
-    const today = new Date();
-    const upcomingEvents = events.filter(
-      (event) => new Date(event.date) > today
-    );
-    upcomingEvents.sort((a, b) => new Date(a.date) - new Date(b.date));
-    return upcomingEvents.slice(0, 3);
-  };
-
-  const upcomingEvents = getUpcomingEvents();
 
   return (
     <div className="container">
-      <h1>Home</h1>
-
+      <h1 className="text-center my-4">Eventos</h1>
       <div className="row">
-        {upcomingEvents.map((event) => (
-          <div className="col-md-4" key={event.id}>
-            <UpcomingEventCard
-              image={event.image}
-              title={event.name}
-              date={formatDate(event.date)}
-              time={event.time}
-            />
-          </div>
-        ))}
+        {Array.isArray(events) && events.length > 0 ? (
+          events.map((event) => (
+            <div key={event.id} className="col-lg-4 mb-4">
+              <div className="card h-100">
+                <img
+                  src={event.imagen_evento}
+                  className="card-img-top"
+                  alt={event.nombre_evento}
+                />
+                <div className="card-body">
+                  <h5 className="card-title">{event.nombre_evento}</h5>
+                  <p className="card-text">{event.descripcion}</p>
+                </div>
+                <div className="card-footer">
+                  <NavLink
+                    className="btn btn-primary btn-block"
+                    to={`/event/${event.id}`}
+                  >
+                    Ver detalles
+                  </NavLink>
+                </div>
+              </div>
+            </div>
+          ))
+        ) : (
+          <p className="text-center">No hay datos disponibles</p>
+        )}
       </div>
     </div>
   );
