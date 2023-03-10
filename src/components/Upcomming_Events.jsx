91aeb0c5e0a61d25465ec1327a128736af29a040
Marksolidor <marco350sl@gmail.com>
2023-03-09 22:11:31 -0300

    fix events.json for by mapped and feat the upcomming component for render the 3 upcomming events

--- src/components/Upcomming_Events.jsx
+++ src/components/Upcomming_Events.jsx
@@ -1,34 +1,43 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 
-function UpcomingEvents(props) {
-  const formatDate = (date) => {
-    const options = { year: "numeric", month: "long", day: "numeric" };
-    return new Date(date).toLocaleDateString(undefined, options);
-  };
+function Upcomming_Events(props) {
+  const { events } = props;
 
-  const getUpcomingEvents = () => {
-    const today = new Date();
-    const upcomingEvents = props.events.filter(
-      (event) => new Date(event.fecha) > today
-    );
-    upcomingEvents.sort((a, b) => new Date(a.fecha) - new Date(b.fecha));
-    return upcomingEvents.slice(0, 3);
-  };
+  // Get the current date
+  const currentDate = new Date();
 
-  const upcomingEvents = getUpcomingEvents();
+  // Filter the events array to only include the events happening in the future
+  const upcomingEvents = events.filter(
+    (event) => new Date(event.fecha) >= currentDate
+  );
 
+  // Sort the upcoming events array by date, in ascending order
+  upcomingEvents.sort((a, b) => new Date(a.fecha) - new Date(b.fecha));
+
+  // Get the first 3 upcoming events
+  const firstThreeUpcomingEvents = upcomingEvents.slice(0, 3);
+
+  // Render the upcoming events
   return (
     <div className="row">
-      {upcomingEvents.map((event) => (
+      {firstThreeUpcomingEvents.map((event) => (
         <div className="col-md-4" key={event.id}>
           <div className="card">
+            <NavLink to={`/events/${event.id}`}>
+              <img
+                src={event.imagen_evento}
+                className="card-img-top"
+                alt={event.nombre_evento}
+              />
+            </NavLink>
             <div className="card-body">
               <h5 className="card-title">{event.nombre_evento}</h5>
               <p className="card-text">
-                <strong>Date:</strong> {formatDate(event.fecha)}
+                <strong>Date:</strong> {event.fecha}
               </p>
               <p className="card-text">
-                <strong>Location:</strong> {event.location}
+                <strong>Location:</strong> {event.direccion}
               </p>
             </div>
           </div>
@@ -38,4 +47,4 @@ function UpcomingEvents(props) {
   );
 }
 
-export default UpcomingEvents;
+export default Upcomming_Events;
