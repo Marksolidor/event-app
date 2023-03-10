e5c4da426ab9a1229b55f0ee92d4c157e21b2543
Yarenla Stuardo <104539159+YareStu30@users.noreply.github.com>
2023-03-10 20:52:28 -0300

    Update Register.jsx

--- src/views/Register.jsx
+++ src/views/Register.jsx
@@ -1,11 +1,10 @@
 import { useForm } from "react-hook-form";
-import { edadValidator } from "../components/validators";
+import { edadValidator } from "./validators";
 
 const Registro = () => {
   const {
     register,
     formState: { errors },
-    watch,
     handleSubmit,
   } = useForm({});
 
@@ -13,135 +12,60 @@ const Registro = () => {
     console.log(data);
   };
 
-  const Phone = watch("Phone");
-
   return (
-    <div>
-      <div>
-        <h2>
-          Registrate aquí
-          <img
-            src="https://cdn-icons-png.flaticon.com/512/3148/3148327.png"
-            width="50px"
-            className="flecha"
-            alt="flecha de color celeste"
-          />
-        </h2>
-      </div>
-
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <div>
-          <label>Nombre</label>
-          <input
-            id="txtpas"
-            style={{ textAlign: "center" }}
-            type="text"
-            {...register("nombre", {
-              required: true,
+    
+<form  onSubmit={handleSubmit(onSubmit)} class="row g-3">
+<div class="col-md-6">
+    <label for="inputName" class="form-label">Nombre</label>
+    <input type="text" class="form-control"  {...register("nombre", {
+            required: true,
               maxLength: 10,
-            })}
-          />
-          {errors.nombre?.type === "required" && (
+            })}/>
+            {errors.nombre?.type === "required" && (
             <p>El campo nombre es requerido</p>
           )}
           {errors.nombre?.type === "maxLength" && (
             <p>El campo nombre debe tener menos de 10 caracteres</p>
           )}
-        </div>
-        <div>
-          <label>Apellidos</label>
-          <input
-            type="text"
-            id="txtpas"
-            style={{ textAlign: "center" }}
-            {...register("Apellidos", {
-              required: true,
+  </div>
+  <div class="col-md-6">
+    <label for="inputName" class="form-label">Apellidos</label>
+    <input type="text" class="form-control" {...register("Apellidos", {
+               required: true,
               maxLength: 20,
-            })}
-          />
-        </div>
-        <div>
-          <label>Nickname</label>
-          <input
-            type="text"
-            id="txtpas"
-            style={{ textAlign: "center" }}
-            {...register("Nickname", {
-              required: true,
-              maxLength: 10,
-            })}
-          />
-        </div>
-        <div>
-          <label>Rut</label>
-          <input
-            type="text"
-            id="txtpas"
-            style={{ textAlign: "center" }}
-            {...register("Rut", {
-              required: true,
-            })}
-          />
-        </div>
-        <div>
-          <label>Edad</label>
-          <input
-            type="text"
-            id="txtpas"
-            style={{ textAlign: "center" }}
-            {...register("edad", {
-              validate: edadValidator,
-            })}
-          />
-          {errors.edad && (
-            <p>Tienes que tener más de 18 años para registrarte</p>
-          )}
-        </div>
-        <div>
-          <label>Dirección</label>
-          <input
-            type="text"
-            id="txtpas"
-            style={{ textAlign: "center" }}
-            {...register("Dirección", {
-              required: true,
-            })}
-          />
-        </div>
-        <div>
-          <label>Email</label>
-          <input
-            type="text"
-            id="txtpas"
-            style={{ textAlign: "center" }}
-            {...register("email", {
-              pattern: /^[^\s@]+@[^\s@]+\.[^\s@]+$/i,
-            })}
-          />
-          {errors.email?.type === "pattern" && (
-            <p>El formato del email es incorrecto</p>
-          )}
-        </div>
+             })}/>
+  </div>
+  <div class="col-md-6">
+    <label for="inputNick" class="form-label">Nickname</label>
+    <input type="text" class="form-control" 
+    {...register("Nickname", {
+                    required: true,
+                 maxLength: 10,
+               })}/>
+  </div>
+  
+  <div class="col-md-6">
+    <label for="inputRut" class="form-label">Rut</label>
+    <input type="text" class="form-control" id="inputRut" {...register("Rut", {
+               required: true,
+             })}/>
+  </div>
 
-        <div>
-          <label>Phone</label>
-          <input type="text" {...register("Phone")} />
-        </div>
-        {Phone && (
-          <div>
-            <label>Teléfono</label>
-            <input
-              type="text"
-              id="txtpas"
-              style={{ textAlign: "center" }}
-              {...register("telefono")}
-            />
-          </div>
-        )}
-        <input type="submit" value="Enviar" />
-      </form>
-    </div>
+  <div class="col-md-6">
+    <label for="inputEmail4" class="form-label">Email</label>
+    <input type="email" class="form-control" />
+  </div>
+  <div class="col-md-6">
+    <label for="inputPassword4" class="form-label">Password</label>
+    <input type="password" class="form-control" id="inputPassword4"/>
+  </div>
+  
+  <div class="col-12">
+    <button type="submit" class="btn btn-primary">Sign in</button>
+  </div>
+</form>
+
   );
-};
+ };
 
 export default Registro;
